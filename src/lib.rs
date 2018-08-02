d81553ec8e4ff344c795dd6913bb106c86efe1ec
Darius Jahandarie <djahanda@akamai.com>
2018-08-02 16:52:43 -0400

    Rearrange repo to only contain quickcheck_derive.

--- src/lib.rs
+++ src/lib.rs
@@ -1,88 +1,53 @@
-//! This crate is a port of
-//! [Haskell's QuickCheck](http://hackage.haskell.org/package/QuickCheck).
-//!
-//! For detailed examples, please see the
-//! [README](https://github.com/BurntSushi/quickcheck).
+#![crate_type = "proc-macro"]
+#![recursion_limit = "128"]
 
-#![allow(deprecated)] // for connect -> join in 1.3
+extern crate proc_macro;
+#[macro_use]
+extern crate quote;
+extern crate syn;
 
-#[cfg(feature = "use_logging")]
-extern crate env_logger;
-#[cfg(feature = "use_logging")]
-#[macro_use] extern crate log;
-extern crate rand;
+mod attrs;
+mod structural;
 
-pub use arbitrary::{
-    Arbitrary, Gen, StdGen,
-    empty_shrinker, single_shrinker,
-};
-pub use rand::Rng;
-pub use tester::{QuickCheck, Testable, TestResult, quickcheck};
+use attrs::*;
+use proc_macro::TokenStream;
+use structural::*;
+use syn::{Body, parse_derive_input};
 
-/// A macro for writing quickcheck tests.
-///
-/// This macro takes as input one or more property functions to test, and
-/// produces a proper `#[test]` function for each property. If the property
-/// fails, the behavior is as if `quickcheck` were called on the property
-/// (i.e., it panics and fails the test).
-///
-/// Note that this macro doesn't support `mut` or patterns in parameters.
-///
-/// # Example
-///
-/// ```rust
-/// # #[macro_use] extern crate quickcheck; fn main() {
-/// quickcheck! {
-///     fn prop_reverse_reverse(xs: Vec<usize>) -> bool {
-///         let rev: Vec<_> = xs.clone().into_iter().rev().collect();
-///         let revrev: Vec<_> = rev.into_iter().rev().collect();
-///         xs == revrev
-///     }
-/// };
-/// # }
-/// ```
-#[macro_export]
-macro_rules! quickcheck {
-    (@as_items $($i:item)*) => ($($i)*);
-    {
-        $(
-            $(#[$m:meta])*
-            fn $fn_name:ident($($arg_name:ident : $arg_ty:ty),*) -> $ret:ty {
-                $($code:tt)*
-            }
-        )*
-    } => (
-        quickcheck! {
-            @as_items
-            $(
-                #[test]
-                $(#[$m])*
-                fn $fn_name() {
-                    fn prop($($arg_name: $arg_ty),*) -> $ret {
-                        $($code)*
+#[proc_macro_derive(Arbitrary, attributes(arbitrary))]
+pub fn derive(input: TokenStream) -> TokenStream {
+    let item = parse_derive_input(&input.to_string())
+        .expect("Couldn't parse item");
+    let (arbitrary, shrink) = match item.body {
+        Body::Struct(ref variant) => derive_struct(&item, &variant),
+        Body::Enum(ref variants) => derive_enum(&item, &variants),
+    };
+    let valid = process_attrs(&item);
+
+    let name = &item.ident;
+    let (impl_generics, ty_generics, where_clause) = item.generics.split_for_impl();
+    let ast = quote! {
+        impl ::quickcheck::Arbitrary for #impl_generics #name #ty_generics #where_clause {
+            #[allow(unused_mut, unused_variables)]
+            fn arbitrary<G: ::quickcheck::Gen>(_g: &mut G) -> Self {
+                // TODO Find a way to use "self" instead of "this".
+                let valid = |this: &Self| { #valid };
+                let mut gen = move || { #arbitrary };
+
+                loop {
+                    let out = gen();
+                    if valid(&out) {
+                        return out;
                     }
-                    $crate::quickcheck(prop as fn($($arg_ty),*) -> $ret);
                 }
-            )*
+            }
+
+            fn shrink(&self) -> Box<Iterator<Item=Self>> {
+                #shrink
+            }
         }
-    )
+    };
+    ast.to_string()
+       .parse()
+       .expect("Couldn't parse string to tokens")
 }
-
-#[cfg(feature = "use_logging")]
-fn env_logger_init() -> Result<(), log::SetLoggerError> {
-    env_logger::init()
-}
-
-#[cfg(not(feature = "use_logging"))]
-fn env_logger_init() { }
-#[cfg(not(feature = "use_logging"))]
-macro_rules! info {
-    ($($_ignore:tt)*) => { () };
-}
-
-
-mod arbitrary;
-mod tester;
-
-#[cfg(test)]
-mod tests;
